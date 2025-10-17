1c3dea8ba389c348b6a8171831bd51d5f9961c96
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-10-17 19:03:04 +0900

    feat: niconicoイベントをカレンダーに追加

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Calendar } from './calendar';
+import { getNiconicoEvents, niconicoTargets } from './niconico';
+
+async function main (): Promise<void> {
+  // Niconico
+  for (const target of niconicoTargets) {
+    const calendar = new Calendar('koteiou', await getNiconicoEvents(target));
+    calendar.saveCalendarToFile('dist/niconico', `${target.type}.ical`);
+  }
+}
+
+main();
