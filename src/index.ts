926215b4096adc63f09b0f2a67e9845fcda0acca
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-10-17 20:50:38 +0900

    feat: 実行時に一度distフォルダを削除する

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Calendar } from './calendar';
 import { getNiconicoEvents, niconicoTargets } from './niconico';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 async function main (): Promise<void> {
+  // dist を削除
+  await fs.rm(path.join(__dirname, '../dist'), {
+    recursive: true,
+    force: true
+  });
+
   // Niconico
   for (const target of niconicoTargets) {
     const calendar = new Calendar('koteiou', await getNiconicoEvents(target));
